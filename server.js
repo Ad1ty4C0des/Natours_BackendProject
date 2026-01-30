29a6d57714d342fd838febbea6d5b1f0012b20e6
Aditya Pratap Singh <157611567+Ad1ty4C0des@users.noreply.github.com>
2026-01-30 19:50:46 +0530

    Unhandled Rejections and Uncaught error handling

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', (err) => {
+  console.log('Uncaught Exception ⛔ shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
@@ -20,6 +27,15 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('Unhandled Rejection ⛔ shutting down...');
+  console.log(err.name, err.message);
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
