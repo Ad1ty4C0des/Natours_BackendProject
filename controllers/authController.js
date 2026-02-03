3cfa6def74e77788e7024e54b9aed790f026bd38
Aditya Pratap Singh <157611567+Ad1ty4C0des@users.noreply.github.com>
2026-02-03 19:53:44 +0530

    Authorization User Roles and Permissions

--- controllers/authController.js
+++ controllers/authController.js
@@ -88,3 +88,16 @@ exports.protected = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+exports.restrictTo =
+  (...roles) =>
+  (req, res, next) => {
+    //roles allowed - ['admin', 'lead-guide']. role='user'
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403),
+      );
+    }
+
+    next();
+  };
