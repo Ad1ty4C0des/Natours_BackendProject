cd519fa281c733a06f4d067c5d8e6c15269505ba
Aditya Pratap Singh <157611567+Ad1ty4C0des@users.noreply.github.com>
2026-02-10 03:02:02 +0530

    Implementing rate limiting

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/extensions */
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError.js');
 const globalErrorHandler = require('./controllers/errorController.js');
@@ -12,6 +13,13 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 100,
+  message: 'Too may requests from this IP, please try again in an hour',
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
