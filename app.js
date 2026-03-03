cf21682d8206c332ccf3901f44cbe668096acc62
Aditya Pratap Singh <157611567+Ad1ty4C0des@users.noreply.github.com>
2026-03-04 01:33:28 +0530

    Modelling Reviews and populating

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const AppError = require('./utils/appError.js');
 const globalErrorHandler = require('./controllers/errorController.js');
 const tourRouter = require('./routes/tourRoutes.js');
 const userRouter = require('./routes/userRoutes.js');
+const reviewRouter = require('./routes/reviewRoutes.js');
 
 const app = express();
 
@@ -73,6 +74,7 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
   // res.status(404).json({
---
+++ routes/reviewRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const reviewController = require('./../controllers/reviewController');
+const authController = require('./../controllers/authController');
+
+const router = express.Router();
+
+router
+  .route('/')
+  .get(authController.protected, reviewController.getAllReviews)
+  .post(
+    authController.protected,
+    authController.restrictTo('user'),
+    reviewController.createReviews,
+  );
+
+module.exports = router;
