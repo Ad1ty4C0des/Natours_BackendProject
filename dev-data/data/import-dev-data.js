b5af21c5155fe909cdc5598beadcb82e4e073e29
Aditya Pratap Singh <157611567+Ad1ty4C0des@users.noreply.github.com>
2026-03-18 23:26:16 +0530

    Improving Read Performance with Indexes

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -14,7 +16,9 @@ mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindandModify: false,
+    useFindAndModify: false,
+    useUnifiedTopology: true, // Add this for a more stable connection logic
+    serverSelectionTimeoutMS: 5000,
   })
   .then(() => {
     console.log('MongoDB connected successfully!');
@@ -22,12 +26,18 @@ mongoose
 
 //read json file
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'),
+);
 
 //import data into db
 
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data successfully loaded');
   } catch (error) {
     console.log(error);
@@ -39,6 +49,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data deleted Successfully');
   } catch (error) {
     console.log(error);
