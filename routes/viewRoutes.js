ce6b88fef90033f78f9ae9bdf3b168c10902943f
Aditya Pratap Singh <157611567+Ad1ty4C0des@users.noreply.github.com>
2026-03-31 04:57:21 +0530

    Updating User data (withour API)

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -9,4 +9,10 @@ router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protected, viewsController.getAccount);
 
+router.post(
+  '/submit-user-data',
+  authController.protected,
+  viewsController.updateUserData,
+);
+
 module.exports = router;
