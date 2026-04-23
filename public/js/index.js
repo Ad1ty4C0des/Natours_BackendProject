55adc1706d972974b425a58a6f604e6febddc5b5
Aditya Pratap Singh <157611567+Ad1ty4C0des@users.noreply.github.com>
2026-04-24 00:26:33 +0530

    Implemented Stripe Messages

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,7 @@ import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { udpateSettings } from './updateSettings';
 import { bookTour } from './stripe';
+import { showAlert } from './alerts';
 
 //DOM Elements
 const mapBox = document.getElementById('map');
@@ -72,3 +73,6 @@ if (bookBtn)
     const { tourId } = e.target.dataset;
     bookTour(tourId);
   });
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alertMessage) showAlert('success', alertMessage, 7);
